ba7d505922e38dda408314622d4f1ec87f1c6c27
Edgar Chan <edgarchancarrillo@gmail.com>
2013-06-05 05:51:00 -0500

    NetClient SSL support implementation, mainly delegate to the internal class

--- src/main/scala/org/vertx/scala/core/net/NetClient.scala
+++ src/main/scala/org/vertx/scala/core/net/NetClient.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011-2013 the original author or authors.
+ * Copyright 2013 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -24,7 +24,7 @@ import org.vertx.java.core.impl.DefaultFutureResult
 
 
 /**
- * @author swilliams
+ * @author Edgar Chan
  * 
  */
 object NetClient {
@@ -32,8 +32,7 @@ object NetClient {
     new NetClient(actual)
 }
 
-//TODO: ClientSSLSupport, TCPSupport extends ClientSSLSupport[NetClient] with TCPSupport[NetClient]
-class NetClient(internal: JNetClient) {
+class NetClient(internal: JNetClient) extends ClientSSLSupport[NetClient] with TCPSupport[NetClient]{
 
   def connect(port: Int, handler: AsyncResult[NetSocket] => Unit): NetClient = {
     connect(port, "localhost", handler)
@@ -49,7 +48,6 @@ class NetClient(internal: JNetClient) {
     this
   }
 
-
   def close(): Unit = internal.close()
 
   def connectTimeout(): Long = internal.getConnectTimeout
@@ -63,48 +61,174 @@ class NetClient(internal: JNetClient) {
     this
   }
 
-  def keyStorePath(keyStorePath: String): NetClient.this.type = {
+  def keyStorePath(keyStorePath: String): NetClient = {
     internal.setKeyStorePath(keyStorePath)
     this
   }
 
-  def reconnectAttempts():Int = internal.getReconnectAttempts()
+  def reconnectAttempts():Int = internal.getReconnectAttempts
 
-  def reconnectInterval():Long = internal.getReconnectInterval()
+  def reconnectInterval():Long = internal.getReconnectInterval
 
-  def receiveBufferSize():Int = internal.getReceiveBufferSize()
+  def receiveBufferSize():Int = internal.getReceiveBufferSize
 
-  def receiveBufferSize(receiveBufferSize: Int):NetClient.this.type = {
+  def receiveBufferSize(receiveBufferSize: Int):NetClient = {
     internal.setReceiveBufferSize(receiveBufferSize)
     this
   }
 
-  def sendBufferSize(): Int = internal.getSendBufferSize()
+  def sendBufferSize(): Int = internal.getSendBufferSize
 
-  def sendBufferSize(sendBufferSize: Int): NetClient.this.type = {
+  def sendBufferSize(sendBufferSize: Int): NetClient= {
     internal.setSendBufferSize(sendBufferSize)
     this
   }
 
-  def trafficClass(): Int = internal.getTrafficClass()
+  def trafficClass(): Int = internal.getTrafficClass
 
-  def trafficClass(trafficClass: Int): NetClient.this.type = {
+  def trafficClass(trafficClass: Int): NetClient= {
     internal.setTrafficClass(trafficClass)
     this
   }
 
-  def trustStorePassword(): String = internal.getTrustStorePassword()
+  def trustStorePassword(): String = internal.getTrustStorePassword
 
-  def trustStorePassword(password: String): NetClient.this.type = {
+  def trustStorePassword(password: String): NetClient= {
     internal.setTrustStorePassword(password)
     this
   }
 
-  def trustStorePath(): String = internal.getTrustStorePath()
+  def trustStorePath(): String = internal.getTrustStorePath
 
-  def trustStorePath(path: String): NetClient.this.type = {
+  def trustStorePath(path: String): NetClient = {
     internal.setTrustStorePath(path)
     this
   }
 
+  def setSSL(ssl: Boolean): NetClient = {
+    internal.setSSL(ssl)
+    this
+  }
+
+  def isSSL: Boolean = {
+    internal.isSSL
+  }
+
+  def setKeyStorePath(path: String): NetClient = {
+    internal.setKeyStorePassword(path)
+    this
+  }
+
+  def getKeyStorePath: String = {
+    internal.getKeyStorePassword
+  }
+
+  def setKeyStorePassword(pwd: String): NetClient = {
+    internal.setKeyStorePassword(pwd)
+    this
+  }
+
+  def getKeyStorePassword: String = {
+    internal.getKeyStorePassword
+  }
+
+  def setTrustStorePath(path: String): NetClient = {
+    internal.setKeyStorePath(path)
+    this
+  }
+
+  def getTrustStorePath: String = {
+    internal.getTrustStorePassword
+  }
+
+  def setTrustStorePassword(pwd: String): NetClient = {
+    internal.setTrustStorePassword(pwd)
+    this
+  }
+
+  def getTrustStorePassword: String = {
+    internal.getTrustStorePassword
+  }
+
+  def setTrustAll(trustAll: Boolean): NetClient = {
+    internal.setTrustAll(trustAll)
+    this
+  }
+
+  def isTrustAll: Boolean = {
+    internal.isTrustAll
+  }
+
+  def setTCPNoDelay(tcpNoDelay: Boolean): NetClient = {
+    internal.isTCPNoDelay
+    this
+  }
+
+  def setSendBufferSize(size: Int): NetClient = {
+    internal.setSendBufferSize(size)
+    this
+  }
+
+  def setReceiveBufferSize(size: Int): NetClient = {
+    internal.setReceiveBufferSize(size)
+    this
+  }
+
+  def setTCPKeepAlive(keepAlive: Boolean): NetClient = {
+    internal.setTCPKeepAlive(keepAlive)
+    this
+  }
+
+  def setReuseAddress(reuse: Boolean): NetClient = {
+    internal.setReuseAddress(reuse)
+    this
+  }
+
+  def setSoLinger(linger: Int): NetClient = {
+    internal.setSoLinger(linger)
+    this
+  }
+
+  def setTrafficClass(trafficClass: Int): NetClient = {
+    internal.setTrafficClass(trafficClass)
+    this
+  }
+
+  def setUsePooledBuffers(pooledBuffers: Boolean): NetClient = {
+    internal.setUsePooledBuffers(pooledBuffers)
+    this
+  }
+
+  def isTCPNoDelay: Boolean = {
+    internal.isTCPNoDelay
+  }
+
+  def getSendBufferSize: Int = {
+    internal.getSendBufferSize
+  }
+
+  def getReceiveBufferSize: Int = {
+    internal.getReceiveBufferSize
+  }
+
+  def isTCPKeepAlive: Boolean = {
+    internal.isTCPKeepAlive
+  }
+
+  def isReuseAddress: Boolean = {
+    internal.isReuseAddress
+  }
+
+  def getSoLinger: Int = {
+    internal.getSoLinger
+  }
+
+  def getTrafficClass: Int = {
+    internal.getTrafficClass
+  }
+
+  def isUsePooledBuffers: Boolean = {
+    internal.isUsePooledBuffers
+  }
 }
+
