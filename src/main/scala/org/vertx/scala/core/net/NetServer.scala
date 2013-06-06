43e2e7a6733f4e32c5388137432ff646c26d31ad
Edgar Chan <edgarchancarrillo@gmail.com>
2013-06-06 05:35:14 -0500

    NetServer re work .. SSL support implementation, mainly delegate to the internal class

--- src/main/scala/org/vertx/scala/core/net/NetServer.scala
+++ src/main/scala/org/vertx/scala/core/net/NetServer.scala
@@ -18,7 +18,7 @@ package org.vertx.scala.core.net
 
 import org.vertx.scala.core.FunctionConverters._
 import org.vertx.java.core.net.{NetServer => JNetServer}
-import org.vertx.java.core.{Handler => JHandler, AsyncResult => JAsyncResult}
+import org.vertx.java.core.{Handler => JHandler, AsyncResult => JAsyncResult, ServerTCPSupport, ServerSSLSupport}
 
 /**
  * @author swilliams
@@ -29,25 +29,26 @@ object NetServer {
 
 }
 
-class NetServer(val internal: JNetServer) extends SocketConfigurer {
+class NetServer(val internal: JNetServer) extends ServerSSLSupport[NetServer] with ServerTCPSupport[NetServer] {
 
-  def close():Unit = internal.close
 
-  def close(handler: () => Unit):Unit = 
-    internal.close(handler)
+  def connectHandler(handler: (NetSocket) => Unit):NetServer = {
+    internal.connectHandler(ConnectHandler(handler))
+    this
+  }
 
-  def listen(port: Int):NetServer.this.type = {
+  def listen(port: Int):NetServer = {
     internal.listen(port)
     this
   }
 
-  def listen(port: Int, address: String):NetServer.this.type = {
-    internal.listen(port, address)
+
+  def listen(port: Int, host: String):NetServer = {
+    internal.listen(port, host)
     this
   }
 
-
-  def listen(port: Int, handler: JAsyncResult[JNetServer] => Unit):NetServer.this.type = {
+  def listen(port: Int, handler: JAsyncResult[JNetServer] => Unit):NetServer = {
     internal.listen(port,  new JHandler[JAsyncResult[JNetServer]]() {
        override def handle(result: JAsyncResult[JNetServer]) = {
          handler(result)
@@ -56,65 +57,161 @@ class NetServer(val internal: JNetServer) extends SocketConfigurer {
      this
   }
 
+  def listen(port: Int, host:String,  handler: JAsyncResult[JNetServer] => Unit):NetServer = {
+    internal.listen(port,  host, new JHandler[JAsyncResult[JNetServer]]() {
+       override def handle(result: JAsyncResult[JNetServer]) = {
+         handler(result)
+       }
+    })
+     this
+  }
 
-  def connectHandler(handler: (NetSocket) => Unit):NetServer.this.type = {
-    internal.connectHandler(ConnectHandler(handler))
+
+  def close:Unit = internal.close
+
+  def close(handler: () => Unit):Unit = 
+    internal.close(handler)
+
+  def port:Int={
+    internal.port
+  }
+
+  def host:String={
+    internal.host
+  }
+
+  def setTCPNoDelay(tcpNoDelay: Boolean): NetServer = {
+    internal.setTCPNoDelay(tcpNoDelay)
     this
   }
 
-  def acceptBacklog():Int = internal.getAcceptBacklog
-
-  def acceptBacklog(backlog: Int):NetServer.this.type = {
-    internal.setAcceptBacklog(backlog)
+  def setSendBufferSize(size: Int): NetServer = {
+    internal.setSendBufferSize(size)
     this
   }
 
-  def keyStorePassword():String = internal.getKeyStorePassword
-
-  def keyStorePassword(keyStorePassword: String):NetServer.this.type = {
-    internal.setKeyStorePassword(keyStorePassword)
+  def setReceiveBufferSize(size: Int): NetServer = {
+    internal.setReceiveBufferSize(size)
     this
   }
 
-  def keyStorePath():String = internal.getKeyStorePath
-
-  def keyStorePath(keyStorePath: String):NetServer.this.type = {
-    internal.setKeyStorePath(keyStorePath)
+  def setTCPKeepAlive(keepAlive: Boolean): NetServer = {
+    internal.setTCPKeepAlive(keepAlive)
     this
   }
 
-  def receiveBufferSize():Int = internal.getReceiveBufferSize
-
-  def receiveBufferSize(receiveBufferSize: Int):NetServer.this.type = {
-    internal.setReceiveBufferSize(receiveBufferSize)
+  def setReuseAddress(reuse: Boolean): NetServer = {
+    internal.setReuseAddress(reuse)
     this
   }
 
-  def sendBufferSize():Int = internal.getSendBufferSize
-
-  def sendBufferSize(sendBufferSize: Int):NetServer.this.type = {
-    internal.setSendBufferSize(sendBufferSize)
+  def setSoLinger(linger: Int): NetServer = {
+    internal.setSoLinger(linger)
     this
   }
 
-  def trafficClass():Int = internal.getTrafficClass
-
-  def trafficClass(trafficClass: Int):NetServer.this.type = {
+  def setTrafficClass(trafficClass: Int): NetServer = {
     internal.setTrafficClass(trafficClass)
     this
   }
 
-  def trustStorePassword():String = internal.getTrustStorePassword
-
-  def trustStorePassword(password: String):NetServer.this.type = {
-    internal.setTrustStorePassword(password)
+  def setUsePooledBuffers(pooledBuffers: Boolean): NetServer = {
+    internal.setUsePooledBuffers(pooledBuffers)
     this
   }
 
-  def trustStorePath():String = internal.getTrustStorePath
+  def isTCPNoDelay: Boolean = {
+    internal.isTCPNoDelay
+  }
 
-  def trustStorePath(path: String):NetServer.this.type = {
+  def getSendBufferSize: Int = {
+    internal.getSendBufferSize
+  }
+
+  def getReceiveBufferSize: Int = {
+    internal.getReceiveBufferSize
+  }
+
+  def isTCPKeepAlive: Boolean = {
+    internal.isTCPKeepAlive
+  }
+
+  def isReuseAddress: Boolean = {
+    internal.isReuseAddress
+  }
+
+  def getSoLinger: Int = {
+    internal.getSoLinger
+  }
+
+  def getTrafficClass: Int = {
+    internal.getTrafficClass
+  }
+
+  def isUsePooledBuffers: Boolean = {
+    internal.isUsePooledBuffers
+  }
+
+  def setAcceptBacklog(backlog: Int): NetServer = {
+    internal.setAcceptBacklog(backlog)
+    this
+  }
+
+  def getAcceptBacklog: Int = {
+    internal.getAcceptBacklog
+  }
+
+  def setSSL(ssl: Boolean): NetServer = {
+    internal.setSSL(ssl)
+    this
+  }
+
+  def isSSL: Boolean = {
+    internal.isSSL
+  }
+
+  def setKeyStorePath(path: String): NetServer = {
+    internal.setKeyStorePath(path)
+    this
+  }
+
+  def getKeyStorePath: String = {
+    internal.getKeyStorePath
+  }
+
+  def setKeyStorePassword(pwd: String): NetServer = {
+    internal.setKeyStorePassword(pwd)
+    this
+  }
+
+  def getKeyStorePassword: String = {
+    internal.getKeyStorePassword
+  }
+
+  def setTrustStorePath(path: String): NetServer = {
     internal.setTrustStorePath(path)
     this
+  }
+
+  def getTrustStorePath: String = {
+    internal.getTrustStorePath
+  }
+
+  def setTrustStorePassword(pwd: String): NetServer = {
+    internal.setTrustStorePassword(pwd)
+    this
+  }
+
+  def getTrustStorePassword: String = {
+    internal.getTrustStorePassword
+  }
+
+  def setClientAuthRequired(required: Boolean): NetServer = {
+    internal.setClientAuthRequired(required)
+    this
+  }
+
+  def isClientAuthRequired: Boolean = {
+    internal.isClientAuthRequired
   }
 }
