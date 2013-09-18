81e37bcfc348aa76b004a1ad67dfc95326fe5f4b
Joern Bernhardt <joern.bernhardt@googlemail.com>
2013-09-19 01:32:37 +0200

    test runner for scala (not working) I don't know why it throws a ClassCastException during the test:
        org.vertx.scala.tests.core.http.ScalaHttpTest > testCreateServer
    FAILED
            org.vertx.java.platform.PlatformManagerException: Failed to
    instantiate verticle factory
        
                Caused by:
                java.lang.ClassCastException:
    org.vertx.scala.platform.impl.ScalaVerticleFactory cannot be cast to
    org.vertx.java.platform.VerticleFactory

---
+++ src/test/scala/org/vertx/scala/tests/core/http/ScalaHttpTest.scala
@@ -0,0 +1,50 @@
+package org.vertx.scala.tests.core.http
+
+import org.vertx.scala.testtools.TestVerticle
+import org.junit.Test
+import org.vertx.testtools.VertxAssert._
+import org.vertx.scala.core.Vertx
+import org.vertx.scala.core.AsyncResult
+import org.vertx.scala.core.http.HttpServer
+
+class ScalaHttpTest extends TestVerticle {
+  println("init scala http test")
+
+  val testPort = 8844
+
+  val html = <html>
+               <head>
+                 <title>test</title>
+               </head>
+               <body>
+                 <h1>Hello world!</h1>
+               </body>
+             </html>.toString()
+
+  @Test
+  def testCreateServer() {
+    println("testCreateServer start")
+    //val vertx = Vertx(this.vertx)
+    vertx.createHttpServer.requestHandler({ req =>
+      println("got a request")
+      req.response.end(html)
+    }).listen(testPort, { ar: AsyncResult[HttpServer] =>
+      if (ar.succeeded()) {
+        println("create http client")
+        vertx.createHttpClient.setPort(testPort).exceptionHandler({ ex =>
+          fail("Should not get an exception, but got " + ex.toString())
+        }).get("/", { resp =>
+          println("got a resonse")
+          resp.bodyHandler({ buf =>
+            assertEquals(html, buf.toString)
+            testComplete()
+          })
+        }).end()
+      } else {
+        fail("listening did not succeed: " + ar.cause().getMessage())
+      }
+    })
+
+    println("done creating server and client")
+  }
+}
