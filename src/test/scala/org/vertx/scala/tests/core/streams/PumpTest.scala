01da37e5ab51a5b647474daa61e3176774a5ada1
Joern Bernhardt <joern.bernhardt@googlemail.com>
2013-10-01 11:47:32 +0200

    pump tests

--- src/test/scala/org/vertx/scala/tests/core/streams/PumpTest.scala
+++ src/test/scala/org/vertx/scala/tests/core/streams/PumpTest.scala
@@ -8,6 +8,7 @@ import org.vertx.scala.core.http.{ HttpClient, HttpClientResponse, HttpServer, H
 import org.vertx.scala.core.streams.Pump
 import org.vertx.scala.testtools.{ ScalaClassRunner, TestVerticle }
 import org.vertx.testtools.VertxAssert._
+import org.vertx.scala.core.file.AsyncFile
 
 class PumpTest extends TestVerticle {
 
@@ -45,17 +46,60 @@ class PumpTest extends TestVerticle {
   }
 
   @Test
-  def pumpingFile(): Unit = fail("not implemented")
+  def pumpingFile(): Unit = withPumpFiles { (filename, oldFile, newFile) =>
+
+    oldFile.endHandler({
+      assertEquals(vertx.fileSystem.propsSync(filename).size, vertx.fileSystem.propsSync(filename + ".copy").size)
+      vertx.fileSystem.deleteSync(filename + ".copy")
+      testComplete
+    })
+
+    Pump.createPump(oldFile, newFile).start
+  }
 
   @Test
-  def pumpStopAndResume(): Unit = fail("not implemented")
+  def pumpStopAndResume(): Unit = withPumpFiles { (filename, oldFile, newFile) =>
+    val filesize = vertx.fileSystem.propsSync(filename).size
+    val pump = Pump.createPump(oldFile, newFile)
+    pump.setWriteQueueMaxSize((filesize / 10).intValue)
+
+    for (i <- 0 to 9) {
+      pump.start
+      pump.stop
+    }
+
+    oldFile.endHandler({
+      val filesizeNew = vertx.fileSystem.propsSync(filename + ".copy").size
+      val oldContents = vertx.fileSystem.readFileSync(filename)
+      val newContents = vertx.fileSystem.readFileSync(filename + ".copy")
+      assertEquals("Filesize should match", filesize, filesizeNew)
+      assertEquals("Filesize should match pumped bytes", filesize, pump.bytesPumped)
+      assertEquals("Contents should match", oldContents, newContents)
+      testComplete
+    })
+    pump.start
+  }
+
+  private def withPumpFiles(fn: (String, AsyncFile, AsyncFile) => Unit): Unit = {
+    val file = "src/test/resources/pumpfile.txt"
+    if (vertx.fileSystem.existsSync(file + ".copy")) {
+      vertx.fileSystem.deleteSync(file + ".copy")
+    }
+    vertx.fileSystem.createFileSync(file + ".copy")
+    vertx.fileSystem.open(file, { ar1: AsyncResult[AsyncFile] =>
+      assertTrue("Should be able to open " + file, ar1.succeeded())
+      val oldFile = ar1.result()
+      vertx.fileSystem.open(file + ".copy", { ar2: AsyncResult[AsyncFile] =>
+        assertTrue("Should be able to open " + file + ".copy", ar2.succeeded())
+        val newFile = ar2.result()
+        fn(file, oldFile, newFile)
+      })
+    })
+  }
 
   private def regularRequestHandler: HttpServerRequest => Unit = { req =>
     req.response.setChunked(true)
-    req.bodyHandler({ buf => println("buf-body:" + buf) })
-    req.dataHandler({ buf => println("buf:" + buf) })
     req.endHandler({ req.response.end })
-    //    req.response.drainHandler(req.response.end)
     Pump.createPump(req, req.response).start
   }
 
