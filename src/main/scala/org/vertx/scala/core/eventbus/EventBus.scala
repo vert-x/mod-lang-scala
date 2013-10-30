e4212d05d7bfba134a02de2ec803d565f41fd679
Joern Bernhardt <joern.bernhardt@googlemail.com>
2013-10-30 23:37:09 +0100

    eventbus send and reply timeouts

--- src/main/scala/org/vertx/scala/core/eventbus/EventBus.scala
+++ src/main/scala/org/vertx/scala/core/eventbus/EventBus.scala
@@ -16,13 +16,44 @@
 package org.vertx.scala.core.eventbus
 
 import scala.collection.concurrent.Map
-
 import org.vertx.java.core.eventbus.{ EventBus => JEventBus, Message => JMessage }
 import org.vertx.scala.VertxWrapper
 import org.vertx.scala.core.{ AsyncResult, Handler }
-import org.vertx.scala.core.FunctionConverters.fnToHandler
+import org.vertx.scala.core.FunctionConverters._
+import org.vertx.java.core.AsyncResultHandler
+import org.vertx.java.core.eventbus.ReplyException
+import org.vertx.java.core.impl.DefaultFutureResult
 
 /**
+ * A distributed lightweight event bus which can encompass multiple vert.x instances.
+ * The event bus implements publish / subscribe, point to point messaging and request-response messaging.<p>
+ * Messages sent over the event bus are represented by instances of the {@link Message} class.<p>
+ * For publish / subscribe, messages can be published to an address using one of the {@link #publish} methods. An
+ * address is a simple {@code String} instance.<p>
+ * Handlers are registered against an address. There can be multiple handlers registered against each address, and a particular handler can
+ * be registered against multiple addresses. The event bus will route a sent message to all handlers which are
+ * registered against that address.<p>
+ * For point to point messaging, messages can be sent to an address using one of the {@link #send} methods.
+ * The messages will be delivered to a single handler, if one is registered on that address. If more than one
+ * handler is registered on the same address, Vert.x will choose one and deliver the message to that. Vert.x will
+ * aim to fairly distribute messages in a round-robin way, but does not guarantee strict round-robin under all
+ * circumstances.<p>
+ * All messages sent over the bus are transient. On event of failure of all or part of the event bus messages
+ * may be lost. Applications should be coded to cope with lost messages, e.g. by resending them, and making application
+ * services idempotent.<p>
+ * The order of messages received by any specific handler from a specific sender should match the order of messages
+ * sent from that sender.<p>
+ * When sending a message, a reply handler can be provided. If so, it will be called when the reply from the receiver
+ * has been received. Reply messages can also be replied to, etc, ad infinitum<p>
+ * Different event bus instances can be clustered together over a network, to give a single logical event bus.<p>
+ * Instances of EventBus are thread-safe.<p>
+ * If handlers are registered from an event loop, they will be executed using that same event loop. If they are
+ * registered from outside an event loop (i.e. when using Vert.x embedded) then Vert.x will assign an event loop
+ * to the handler and use it to deliver messages to that handler.
+ *
+ * @author <a href="http://tfox.org">Tim Fox</a>
+ * @author swilliams
+ * @author Edgar Chan
  * @author <a href="http://www.campudus.com/">Joern Bernhardt</a>
  */
 class EventBus(protected[this] val internal: JEventBus) extends VertxWrapper {
@@ -30,71 +61,160 @@ class EventBus(protected[this] val internal: JEventBus) extends VertxWrapper {
 
   sealed private trait SendOrPublish
   private case class Publish(address: String, value: MessageData) extends SendOrPublish
-  private case class Send[X](address: String, value: MessageData, replyHandler: Option[Handler[JMessage[X]]]) extends SendOrPublish
+  private case class Send[X](address: String, value: MessageData,
+    replyHandler: Option[Either[Handler[JMessage[X]], Handler[AsyncResult[JMessage[X]]]]])
+    extends SendOrPublish
 
+  /**
+   * Publish a message.
+   * @param address The address to publish it to
+   * @param message The message
+   */
   def publish[T <% MessageData](address: String, value: T): EventBus =
-    sendOrPublish(Publish(address, value))
+    sendOrPublish(Publish(address, value), -1)
 
+  /**
+   * Send a message.
+   * @param address The address to send it to
+   * @param message The message
+   */
   def send[T <% MessageData](address: String, value: T): EventBus =
-    sendOrPublish(Send(address, value, None))
+    sendOrPublish(Send(address, value, None), -1)
 
-  def send[T <% MessageData](address: String, value: T, handler: Message[T] => Unit): EventBus = {
-    sendOrPublish(Send(address, value, Some(mapHandler(handler))))
+  /**
+   * Send a message.
+   * @param address The address to send it to
+   * @param message The message
+   * @param replyHandler Reply handler will be called when any reply from the recipient is received
+   */
+  def send[ST <% MessageData, RT <% MessageData](address: String, value: ST, handler: Message[RT] => Unit): EventBus = {
+    sendOrPublish(Send(address, value, Some(Left(mapHandler(handler)))), -1)
   }
 
+  /**
+   * Send a character as a message
+   * @param address The address to send it to
+   * @param message The message
+   * @param timeout - Timeout in ms. If no reply received within the timeout then the reply handler will be unregistered
+   * @param replyHandler Reply handler will be called when any reply from the recipient is received
+   */
+  def sendWithTimeout[ST <% MessageData, RT <% MessageData](address: String, value: ST, timeout: Long, replyHandler: AsyncResult[Message[RT]] => Unit): EventBus =
+    sendOrPublish(Send(address, value, Some(Right(convertArHandler(replyHandler)))), timeout)
+
+  /**
+   * Close the EventBus and release all resources.
+   *
+   * @param doneHandler
+   */
   def close(doneHandler: AsyncResult[Void] => Unit): Unit = internal.close(doneHandler)
 
+  /**
+   * Registers a handler against the specified address.
+   * @param address The address to register it at.
+   * @param handler The handler.
+   */
   def registerUnregisterableHandler[X](address: String, handler: Handler[org.vertx.java.core.eventbus.Message[X]]): EventBus = wrap({
     internal.registerHandler(address, handler)
   })
 
+  /**
+   * Registers a handler against the specified address.
+   * @param address The address to register it at.
+   * @param handler The handler.
+   * @param resultHandler Optional completion handler. If specified, when the register has been
+   * propagated to all nodes of the event bus, the handler will be called.
+   */
   def registerUnregisterableHandler[X](address: String, handler: Handler[org.vertx.java.core.eventbus.Message[X]], resultHandler: AsyncResult[Void] => Unit): EventBus = wrap({
     internal.registerHandler(address, handler, resultHandler)
   })
 
   /**
-   * Please bear in mind that you cannot unregister handlers you register with this method.
+   * Registers a handler against the specified address. Please bear in mind that you cannot
+   * unregister handlers you register with this method.
+   * @param address The address to register it at.
+   * @param handler The handler.
    */
-  def registerHandler[T <% MessageData](address: String, handler: Message[T] => Unit): EventBus = wrap({
-    internal.registerHandler(address, fnToHandler(handler.compose(Message.apply)))
-  })
+  def registerHandler[T <% MessageData](address: String, handler: Message[T] => Unit): EventBus =
+    wrap(internal.registerHandler(address, fnToHandler(handler.compose(Message.apply))))
 
   /**
-   * Please bear in mind that you cannot unregister handlers you register with this method.
+   * Registers a handler against the specified address. Please bear in mind that you cannot
+   * unregister handlers you register with this method.
+   * @param address The address to register it at.
+   * @param handler The handler.
+   * @param resultHandler Optional completion handler. If specified, when the register has been
+   * propagated to all nodes of the event bus, the handler will be called.
    */
   def registerHandler[T <% MessageData](address: String, handler: Message[T] => Unit, resultHandler: AsyncResult[Void] => Unit): EventBus = wrap({
     internal.registerHandler(address, handler.compose(Message.apply), resultHandler)
   })
 
+  /**
+   * Registers a local handler against the specified address. The handler info won't
+   * be propagated across the cluster. Please bear in mind that you cannot unregister handlers you
+   * register with this method.
+   * @param address The address to register it at
+   * @param handler The handler
+   */
   def registerLocalHandler[T <% MessageData](address: String, handler: Message[T] => Unit): EventBus = wrap({
     internal.registerLocalHandler(address, handler.compose(Message.apply))
   })
 
+  /**
+   * Unregisters a handler given the address and the handler
+   * @param address The address the handler was registered at
+   * @param handler The handler
+   */
   def unregisterHandler[T <% MessageData](address: String, handler: Handler[JMessage[T]]): EventBus = wrap({
     internal.unregisterHandler(address, handler)
   })
 
+  /**
+   * Unregisters a handler given the address and the handler
+   * @param address The address the handler was registered at
+   * @param handler The handler
+   * @param resultHandler Optional completion handler. If specified, when the unregister has been
+   * propagated to all nodes of the event bus, the handler will be called.
+   */
   def unregisterHandler[T <% MessageData](address: String, handler: Handler[JMessage[T]], resultHandler: AsyncResult[Void] => Unit): EventBus = wrap({
     internal.unregisterHandler(address, handler, resultHandler)
   })
+
+  /**
+   * Sets a default timeout, in ms, for replies. If a messages is sent specify a reply handler
+   * but without specifying a timeout, then the reply handler is timed out, i.e. it is automatically unregistered
+   * if a message hasn't been received before timeout.
+   * The default value for default send timeout is -1, which means "never timeout".
+   * @param timeoutMs
+   */
+  def setDefaultReplyTimeout(timeoutMs: Long): EventBus = wrap(internal.setDefaultReplyTimeout(timeoutMs))
+
+  /**
+   * Return the value for default send timeout.
+   */
+  def getDefaultReplyTimeout(): Long = internal.getDefaultReplyTimeout()
 
   private def mapHandler[T <% MessageData](handler: Message[T] => Unit): Handler[JMessage[T]] = {
     fnToHandler(handler.compose(Message.apply))
   }
 
-  private def sendOrPublish(cmd: SendOrPublish): EventBus = {
+  private def convertArHandler[T <% MessageData](handler: AsyncResult[Message[T]] => Unit): Handler[AsyncResult[JMessage[T]]] = {
+    asyncResultConverter({x: JMessage[T] => Message.apply(x)})(handler)
+  }
+
+  private def sendOrPublish(cmd: SendOrPublish, timeout: Long): EventBus = {
     cmd match {
       case Publish(address, value) => value.publish(internal, address)
       case Send(address, value, None) => value.send(internal, address)
-      case Send(address, value, Some(handler)) => value.send(internal, address, handler)
+      case Send(address, value, Some(Left(handler))) => value.send(internal, address, handler)
+      case Send(address, value, Some(Right(handler))) => value.sendWithTimeout(internal, address, handler, timeout)
     }
     this
   }
 }
 
-/*
- * @author swilliams
- * @author Edgar Chan
+/**
+ * Companion object for EventBus.
  */
 object EventBus {
   def apply(actual: JEventBus) = new EventBus(actual)
---
+++ src/test/scala/org/vertx/scala/tests/core/eventbus/EventBusTimeoutsTest.scala
@@ -0,0 +1,138 @@
+package org.vertx.scala.tests.core.eventbus
+
+import org.vertx.scala.testtools.TestVerticle
+import org.vertx.testtools.VertxAssert._
+import scala.util.Failure
+import scala.util.Try
+import org.vertx.scala.core.eventbus.Message
+import org.vertx.scala.core.FunctionConverters._
+import org.junit.Test
+import scala.util.Success
+import org.vertx.java.core.eventbus.ReplyException
+import org.junit.Ignore
+
+class EventBusTimeoutsTest extends TestVerticle {
+  @Test
+  def replyWithinTimeout(): Unit = {
+    vertx.eventBus.registerHandler("hello", { msg: Message[String] =>
+      vertx.setTimer(100, timer => msg.reply("hello " + msg.body))
+    })
+
+    vertx.eventBus.sendWithTimeout("hello", "reply-within-timeout", 500, {
+      case Success(reply) =>
+        assertEquals("hello reply-within-timeout", reply.body)
+        testComplete
+      case Failure(ex) => fail("Should receive message within time, but got exception: " + ex)
+    }: Try[Message[String]] => Unit)
+  }
+
+  @Test
+  def noReplyWithinTimeout(): Unit = {
+    vertx.eventBus.registerHandler("hello", { msg: Message[String] =>
+      // do not reply at all
+    })
+
+    vertx.eventBus.sendWithTimeout("hello", "no-reply-within-timeout", 100, {
+      case Success(result) => fail("Should not receive a message within time, but got one: " + result.body)
+      case Failure(ex) => testComplete
+    }: Try[Message[String]] => Unit)
+  }
+
+  @Test
+  def noReplyWithinTimeoutNoRegisteredHandler(): Unit = {
+    vertx.eventBus.sendWithTimeout("hello", "missing-handler", 100, {
+      case Success(result) => fail("Should not receive a message within time, but got one: " + result.body)
+      case Failure(ex) => testComplete
+    }: Try[Message[String]] => Unit)
+  }
+
+  @Test
+  def lateReplyAfterTimeout(): Unit = {
+    vertx.eventBus.registerHandler("hello", { msg: Message[String] =>
+      vertx.setTimer(300, timer => msg.reply("hello " + msg.body))
+    })
+
+    vertx.eventBus.sendWithTimeout("hello", "late-reply-within-timeout", 100, {
+      case Success(result) => fail("Should not receive a message within time, but got one: " + result.body)
+      case Failure(ex) =>
+        assertTrue("exception should be of kind ReplyException", ex.isInstanceOf[ReplyException])
+        assertEquals("exception should be a TIMEOUT", "TIMEOUT", ex.asInstanceOf[ReplyException].failureType().name())
+        // Wait to complete test, so if a message still comes, it shouldn't be received here anymore
+        vertx.setTimer(300, timer => testComplete)
+    }: Try[Message[String]] => Unit)
+  }
+
+  @Test
+  def replyReplyWithinTimeout(): Unit = {
+    vertx.eventBus.registerHandler("hello", { msg: Message[String] =>
+      msg.replyWithTimeout("hello " + msg.body, 500, {
+        case Success(reply) =>
+          assertEquals("got it", reply.body)
+          testComplete
+        case Failure(ex) => fail("Should get a reply on reply in time, but got " + ex.getMessage)
+      }: Try[Message[String]] => Unit)
+    })
+
+    vertx.eventBus.sendWithTimeout("hello", "reply-within-timeout", 250, {
+      case Success(reply) =>
+        assertEquals("hello reply-within-timeout", reply.body)
+        reply.reply("got it")
+      case Failure(ex) =>
+        fail("Should receive message within time, but got exception in first send: " + ex)
+    }: Try[Message[String]] => Unit)
+  }
+
+  @Test
+  def replyNoReplyWithinTimeout(): Unit = {
+    vertx.eventBus.registerHandler("hello", { msg: Message[String] =>
+      msg.replyWithTimeout("hello " + msg.body, 100, {
+        case Success(reply) => fail("Should not get a reply in time, but got " + reply.body)
+        case Failure(ex) => testComplete
+      }: Try[Message[String]] => Unit)
+    })
+
+    vertx.eventBus.sendWithTimeout("hello", "no-reply-within-timeout", 200, {
+      case Success(msg) => vertx.setTimer(500, { timer => msg.reply("replying late") })
+      case Failure(ex) => fail("Should receive a message within time at first, but got: " + ex.getMessage())
+    }: Try[Message[String]] => Unit)
+  }
+
+  @Test
+  def replyNoReplyWithinTimeoutNoRegisteredHandler(): Unit = {
+    vertx.eventBus.registerHandler("hello", { msg: Message[String] =>
+      logger.info("received first message")
+      assertEquals("Should receive a first message before sending reply", "missing-handler", msg.body)
+      msg.replyWithTimeout("hello " + msg.body, 200, {
+        case Success(reply) => fail("Should not get a reply in time, but got " + reply.body)
+        case Failure(ex) =>
+          assertTrue("exception should be of kind ReplyException", ex.isInstanceOf[ReplyException])
+          assertEquals("exception should be a NO_HANDLERS", "NO_HANDLERS", ex.asInstanceOf[ReplyException].failureType().name())
+          testComplete
+      }: Try[Message[String]] => Unit)
+      logger.info("replied to message with 'hello " + msg.body + "'")
+    })
+
+    vertx.eventBus.send("hello", "missing-handler")
+  }
+
+  @Test
+  def replyLateReplyAfterTimeout(): Unit = {
+    vertx.eventBus.registerHandler("hello", { msg: Message[String] =>
+      assertEquals("late-reply-within-timeout", msg.body)
+      msg.replyWithTimeout("late-reply", 100, {
+        case Success(result) => fail("Should not receive a message within time, but got one: " + result.body)
+        case Failure(ex) =>
+          assertTrue("exception should be of kind ReplyException", ex.isInstanceOf[ReplyException])
+          assertEquals("exception should be a TIMEOUT", "TIMEOUT", ex.asInstanceOf[ReplyException].failureType().name())
+          // Wait to complete test, so if a message still comes, it shouldn't be received here anymore
+          vertx.setTimer(300, timer => testComplete)
+      }: Try[Message[String]] => Unit)
+    })
+
+    vertx.eventBus.send("hello", "late-reply-within-timeout", { msg: Message[String] =>
+      assertEquals("Should get a quick reply before late reply", "late-reply", msg.body)
+      vertx.setTimer(300, timer => msg.reply("hello " + msg.body))
+    })
+  }
+
+}
