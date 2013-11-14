20b76ec768de3aeb42145161f73b7329f9500c89
Galder Zamarreño <galder@zamarreno.com>
2013-11-14 10:43:51 +0100

    Test HTTP compressed and uncompressed in same class

--- src/test/scala/org/vertx/scala/tests/core/http/HttpTestForSimpleApi.scala
+++ src/test/scala/org/vertx/scala/tests/core/http/HttpTestForSimpleApi.scala
@@ -7,7 +7,7 @@ import org.vertx.scala.tests.util.TestUtils.completeWithArFailed
 import org.vertx.scala.testtools.TestVerticle
 import org.vertx.testtools.VertxAssert.{ assertEquals, fail, testComplete }
 
-trait HttpTestForSimpleApi extends TestVerticle {
+class HttpTestForSimpleApi extends TestVerticle {
   val testPort = 8844
 
   val html = <html>
@@ -19,13 +19,13 @@ trait HttpTestForSimpleApi extends TestVerticle {
                </body>
              </html>.toString()
 
-  def createHttpServer() {
+  @Test def createHttpServer() {
     vertx.createHttpServer.requestHandler(regularRequestHandler).listen(testPort, checkServer({ c =>
-      c.getNow("/", correctHeadAndBodyHandler(testComplete))
+      c.getNow("/", correctHeadAndBodyHandler(c, testComplete))
     }))
   }
 
-  def httpsServer(): Unit = {
+  @Test def httpsServer(): Unit = {
     val server = vertx.createHttpServer.setSSL(true)
 
     server.setKeyStorePath("./src/test/keystores/server-keystore.jks").setKeyStorePassword("wibble")
@@ -35,55 +35,55 @@ trait HttpTestForSimpleApi extends TestVerticle {
       c.setSSL(true)
       c.setKeyStorePath("./src/test/keystores/client-keystore.jks").setKeyStorePassword("wibble")
       c.setTrustStorePath("./src/test/keystores/client-truststore.jks").setTrustStorePassword("wibble")
-      c.exceptionHandler(ex => fail("Should not get exception but got " + ex)) getNow ("/", correctHeadAndBodyHandler(testComplete))
+      c.exceptionHandler(ex => fail("Should not get exception but got " + ex)) getNow ("/", correctHeadAndBodyHandler(c, testComplete))
     }))
   }
 
-  def invalidPort(): Unit = {
+  @Test def invalidPort(): Unit = {
     vertx.createHttpServer.requestHandler({ r => }).listen(1128371831, completeWithArFailed[HttpServer])
   }
 
-  def invalidHost(): Unit = {
+  @Test def invalidHost(): Unit = {
     vertx.createHttpServer.requestHandler({ r => }) listen (testPort, "iqwjdoqiwjdoiqwdiojwd", completeWithArFailed)
   }
 
-  def postMethod(): Unit = {
+  @Test def postMethod(): Unit = {
     vertx.createHttpServer.requestHandler(regularRequestHandler).listen(testPort, checkServer({ c =>
-      c.post("/", correctHeadAndBodyHandler(testComplete)).end()
+      c.post("/", correctHeadAndBodyHandler(c, testComplete)).end()
     }))
   }
 
-  def getMethod(): Unit = {
+  @Test def getMethod(): Unit = {
     vertx.createHttpServer.requestHandler(regularRequestHandler).listen(testPort, checkServer({ c =>
-      c.get("/", correctHeadAndBodyHandler(testComplete)).end()
+      c.get("/", correctHeadAndBodyHandler(c, testComplete)).end()
     }))
   }
 
-  def headMethod(): Unit = {
+  @Test def headMethod(): Unit = {
     vertx.createHttpServer.requestHandler(regularRequestHandler).listen(testPort, checkServer({ c =>
-      c.head("/", correctHeadAndEmptyBodyHandler(testComplete)).end()
+      c.head("/", correctHeadAndEmptyBodyHandler(c, testComplete)).end()
     }))
   }
 
-  def connectMethod(): Unit = {
+  @Test def connectMethod(): Unit = {
     vertx.createHttpServer.requestHandler(regularRequestHandler).listen(testPort, checkServer({ c =>
-      c.connect("/", correctHeadAndEmptyBodyHandler(testComplete)).end()
+      c.connect("/", correctHeadAndEmptyBodyHandler(c, testComplete)).end()
     }))
   }
 
-  def getRequestMethod(): Unit = headAndBodyRequest("GET")
-  def postRequestMethod(): Unit = headAndBodyRequest("POST")
-  def putRequestMethod(): Unit = headAndBodyRequest("PUT")
-  def deleteRequestMethod(): Unit = headAndBodyRequest("DELETE")
-  def headRequestMethod(): Unit = headOnlyRequest("HEAD")
-  def traceRequestMethod(): Unit = headAndBodyRequest("TRACE")
-  def connectRequestMethod(): Unit = headOnlyRequest("CONNECT")
-  def optionsRequestMethod(): Unit = headAndBodyRequest("OPTIONS")
-  def patchRequestMethod(): Unit = headAndBodyRequest("PATCH")
+  @Test def getRequestMethod(): Unit = headAndBodyRequest("GET")
+  @Test def postRequestMethod(): Unit = headAndBodyRequest("POST")
+  @Test def putRequestMethod(): Unit = headAndBodyRequest("PUT")
+  @Test def deleteRequestMethod(): Unit = headAndBodyRequest("DELETE")
+  @Test def headRequestMethod(): Unit = headOnlyRequest("HEAD")
+  @Test def traceRequestMethod(): Unit = headAndBodyRequest("TRACE")
+  @Test def connectRequestMethod(): Unit = headOnlyRequest("CONNECT")
+  @Test def optionsRequestMethod(): Unit = headAndBodyRequest("OPTIONS")
+  @Test def patchRequestMethod(): Unit = headAndBodyRequest("PATCH")
 
-  private def simpleRequest(fn: (() => Unit) => HttpClientResponse => Unit)(name: String): Unit = {
+  private def simpleRequest(fn: (HttpClient, () => Unit) => HttpClientResponse => Unit)(name: String): Unit = {
     vertx.createHttpServer.requestHandler(regularRequestHandler).listen(testPort, checkServer({ c =>
-      c.request(name, "/", fn(testComplete)).end()
+      c.request(name, "/", fn(c, testComplete)).end()
     }))
   }
 
@@ -94,25 +94,25 @@ trait HttpTestForSimpleApi extends TestVerticle {
     req.response.end(html)
   }
 
-  private def checkCorrectHeader(fn: () => Unit) = { resp: HttpClientResponse =>
-    assertEquals(200, resp.statusCode)
-    assertEquals("OK", resp.statusMessage)
+  private def checkCorrectHeader(c: HttpClient, fn: () => Unit) = { resp: HttpClientResponse =>
+    assertEquals(assertMessage(c), 200, resp.statusCode)
+    assertEquals(assertMessage(c), "OK", resp.statusMessage)
     fn()
   }
 
-  private def correctHeadAndBodyHandler(fn: () => Unit) = { resp: HttpClientResponse =>
-    checkCorrectHeader({ () =>
+  private def correctHeadAndBodyHandler(c: HttpClient, fn: () => Unit) = { resp: HttpClientResponse =>
+    checkCorrectHeader(c, { () =>
       resp.bodyHandler({ buf =>
-        assertEquals(html, buf.toString)
+        assertEquals(assertMessage(c), html, buf.toString)
         fn()
       })
     }).apply(resp): Unit
   }
 
-  private def correctHeadAndEmptyBodyHandler(fn: () => Unit) = { resp: HttpClientResponse =>
-    checkCorrectHeader({ () =>
+  private def correctHeadAndEmptyBodyHandler(c: HttpClient, fn: () => Unit) = { resp: HttpClientResponse =>
+    checkCorrectHeader(c, { () =>
       resp.bodyHandler { buf =>
-        assertEquals(0, buf.length)
+        assertEquals(assertMessage(c), 0, buf.length)
         fn()
       }
     }).apply(resp): Unit
@@ -120,13 +120,17 @@ trait HttpTestForSimpleApi extends TestVerticle {
 
   private def checkServer(clientFn: HttpClient => Unit) = { ar: AsyncResult[HttpServer] =>
     if (ar.succeeded()) {
-      val httpClient = vertx.createHttpClient.setPort(testPort)
-      httpClient.setTryUseCompression(compression)
-      clientFn(httpClient)
+      val clients = List(
+        vertx.createHttpClient().setPort(testPort).setTryUseCompression(tryUseCompression = false),
+        vertx.createHttpClient().setPort(testPort).setTryUseCompression(tryUseCompression = true))
+
+      clients.foreach(clientFn)
     } else {
-      fail("listening did not succeed: " + ar.cause().getMessage())
+      fail("listening did not succeed: " + ar.cause().getMessage)
     }
   }
 
-  protected def compression: Boolean
+  private def assertMessage(c: HttpClient) =
+    s"Assert failed for client(compression=$c.getTryUseCompression())"
+
 }
