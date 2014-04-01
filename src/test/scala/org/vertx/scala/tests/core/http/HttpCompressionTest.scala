7efd38a673ca3e4fae454ba420312677a5173f6c
Galder Zamarreño <galder@zamarreno.com>
2014-04-01 17:28:19 +0200

    Restructure HTTP tests and exclude some transitive dependencies

--- src/test/scala/org/vertx/scala/tests/core/http/HttpCompressionTest.scala
+++ src/test/scala/org/vertx/scala/tests/core/http/HttpCompressionTest.scala
@@ -1,27 +1,15 @@
-/*
- * Copyright 2014 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.vertx.scala.tests.core.http
 
 import org.junit.Test
 import org.vertx.scala.tests.util.TestUtils._
 import org.vertx.testtools.VertxAssert._
+import org.vertx.scala.testtools.TestVerticle
+import org.vertx.scala.core.Vertx
 
-class HttpCompressionTest extends HttpTest {
+final class HttpCompressionTest extends TestVerticle with HttpTestBase {
 
-  compression = true
+  override def compression: Boolean = true
+  override def getVertx: Vertx = vertx
 
   @Test override def sendFile(): Unit = {
     val (file, content) = generateRandomContentFile("test-send-file.html", 10000)
